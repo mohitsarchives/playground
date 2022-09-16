ce18c04087b4fa72b8756a2d833f31d76a17af9c
mohitsarchives <mohit.sewani1998@gmail.com>
2022-09-16 16:51:31 +1000

    Python sandbox

--- hello.py
+++ hello.py
@@ -1,11 +1,17 @@
 # Ask a user for their name
-name = input("What's your name? ")
+name = input("What's your name? ").strip().title()
 
 # Remove whitespace from str
-name = name.strip()
+#name = name.strip().title()
 
 # Capitalizes only the first character of name
-name = name.capitalize()
+# name = name.capitalize()
+
+# Capitalizes first character of each word
+# name = name.title()
+
+# Split user's name
+first, last = name.split(" ")
 
 print("Hello,", name)
 
