18e62d6294cf6a751fb9c97568c8dd554dff5587
mohitsarchives <mohit.sewani1998@gmail.com>
2022-09-20 19:10:13 +1000

    Python sandbox

--- number.py
+++ number.py
@@ -1,9 +1,13 @@
-while True:
-    try:
-        x = int(input("What's x? "))
-    except ValueError:
-        print("x is not an integer.")
-    else:
-        break
+def main():
+    x = get_int()
+    print(f"x is {x}")
 
-print(f"x is {x}")
+def get_int():
+    while True:
+        try:
+            return int(input("What's x? "))
+        except ValueError:
+            pass
+
+if __name__ == "__main__":
+    main()
