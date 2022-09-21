67db4f9dd67065d57c278efb307dce624564f821
mohitsarchives <mohit.sewani1998@gmail.com>
2022-09-21 20:55:01 +1000

    Python sandbox

---
+++ sets.py
@@ -0,0 +1,17 @@
+# Create an empty set
+s = set()
+
+# Add elements to the set
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(4)
+s.add(3)
+
+# Remove elements from the set
+s.remove(2)
+
+# Print the elements of the set 
+print(s)
+
+print(f"The set has {len(s)} elements.")
