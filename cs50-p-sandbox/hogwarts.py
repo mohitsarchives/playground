36e8a6ee41dc795b08f8e6d3029d3ecab9eb7a4b
mohitsarchives <mohit.sewani1998@gmail.com>
2022-09-25 12:08:54 +1000

    Python sandbox

--- cs50-p-sandbox/hogwarts.py
+++ cs50-p-sandbox/hogwarts.py
@@ -1,6 +1,9 @@
 students = [
     {"name": "Harry", "house": "Gryffindor", "patronus": "otter"},
     {"name": "Hermione", "house": "Gryffindor", "patronus": "Stag"},
-    {"name": "Ron", "house": "Gryffindor", "patronus", : "Jack Russell terrier"},
-    {"name": "Draco", "house": "Slytherin", "patronus": None}
+    {"name": "Ron", "house": "Gryffindor", "patronus": "Jack Russell terrier"},
+    {"name": "Draco", "house": "Slytherin", "patronus": None},
+    {"name": "Cho", "house": "Gryffindor", "patronus": None}
 ]
+
+print(students[1])
