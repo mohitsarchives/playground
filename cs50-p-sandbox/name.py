afdb36d39de4b1ac8865c5e664da725dec4e7814
mohitsarchives <mohit.sewani1998@gmail.com>
2022-09-26 22:31:27 +1000

    Python sandbox

--- cs50-p-sandbox/name.py
+++ cs50-p-sandbox/name.py
@@ -1,6 +1,8 @@
 import sys
 
-try:
-    print("hello, my name is", sys.argv[1])
-except IndexError:
-    print("please enter your name.")
+if len(sys.argv) < 2:
+    sys.exit("Too few arguments.")
+elif len(sys.argv) > 2:
+    sys.exit("Too many arguments.")
+
+print("Hello, my name is", sys.argv[1])
